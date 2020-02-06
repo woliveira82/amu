0733cd49971e4c41c179bc44aa08641d87aecd6a
Weiner <woliveira82@gmail.com>
2020-02-06 20:51:50 -0300

    Added user creation rule

--- app/models/Token.py
+++ app/models/Token.py
@@ -1,5 +1,7 @@
 from app import db
 from app.inc import Dao
+from secrets import token_urlsafe
+from datetime import datetime, timedelta
 
 
 class Token(db.Model, Dao):
@@ -7,18 +9,18 @@ class Token(db.Model, Dao):
     __tablename__ = 'token'
     
     id = db.Column(db.Integer, primary_key=True)
-    token = db.Column(db.String(32), unique=True)
-    expires_at = db.Column(db.Date, nullable=False)
+    token = db.Column(db.String(32), unique=True, nullable=False)
+    expires_at = db.Column(db.DateTime, nullable=True)
     action = db.Column(db.SmallInteger, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
-    def __init__(self, token, expires_at, action, user_id, id=None):
+    def __init__(self, action, user_id, token=None, expires_at=None, id=None):
         self.id = id
-        self.token = token
-        self.expires_at = expires_at
         self.action = action
         self.user_id = user_id
+        self.token = token if token else token_urlsafe(16)
+        self.expires_at = expires_at if expires_at else datetime.now() + timedelta(days=1)
 
 
     def as_dict(self):
--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,7 @@
 from app import db
 from app.inc import Dao
+from . import Token
+from datetime import datetime
 
 
 class User(db.Model, Dao):
@@ -7,17 +9,19 @@ class User(db.Model, Dao):
     __tablename__ = 'user'
     
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(64), nullable=False)
-    deactivated_at = db.Column(db.Date, default=None)
+    active = db.Column(db.Boolean, nullable=False)
+    deactivated_at = db.Column(db.DateTime, default=None)
     name = db.Column(db.String(256), nullable=False)
     last_name = db.Column(db.String(256))
 
 
-    def __init__(self, email, password, deactivated_at, name, last_name, id=None):
+    def __init__(self, email, password, name, last_name, active=False, deactivated_at=None, id=None):
         self.id = id
         self.email = email
         self.password = password
+        self.active = active
         self.deactivated_at = deactivated_at
         self.name = name     
         self.last_name = last_name
@@ -27,7 +31,20 @@ class User(db.Model, Dao):
         return {
             'id': self.id,
             'email': self.email,
+            'password': self.password,
+            'active': self.active,
             'deactivated_at': self.deactivated_at,
             'name': self.name,
             'last_name': self.last_name,
         }
+
+    
+    def create(self):
+        super().create()
+        
+        token_data = {
+            'action': 1,
+            'user_id': self.id,
+        }
+        token = Token(**token_data)
+        token.create()
